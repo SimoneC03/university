c3f310872699fe453bf00ae2e2de89bf583218cd
SimoneC03 <simoneca3@gmail.com>
2023-01-09 22:27:52 +0100

    validation fix

--- C/exam-training/ex01.c
+++ C/exam-training/ex01.c
@@ -10,7 +10,7 @@ typedef struct
   int M;
 } Arguments;
 
-Arguments checkArgs(int, char*[]);
+Arguments checkArgs(int, char**);
 
 void main(int argc, char *argv[])
 {
@@ -35,12 +35,12 @@ void main(int argc, char *argv[])
   }
 }
 
-Arguments checkArgs(int argc, char *argv[])
+Arguments checkArgs(int argc, char **argv)
 {
-  int k = atoi(argv[1]);
+  int k = (argv[1] != NULL) ? atoi(argv[1]) : -1;
   char w = (argv[2] != NULL) ? argv[2][0] : '-1';
-  int N = atoi(argv[3]);
-  int M = atoi(argv[4]);
+  int N = (argv[3] != NULL) ? atoi(argv[3]) : -1;
+  int M = (argv[4] != NULL) ? atoi(argv[4]) : -1;
   if (k >= 10 && k <= 15 && w >= 'a' && w <= 'z' && N >= 4 && N <= 8 && M >= 4 && M <= 8)
   {
     Arguments arguments = {k, w, N, M};
