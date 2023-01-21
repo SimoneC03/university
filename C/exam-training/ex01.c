dfc99bf915470611903df21220ec6989463e18ef
SimoneC03 <simoneca3@gmail.com>
2023-01-21 20:01:28 +0100

    ✅ ex01 completed

--- C/exam-training/ex01.c
+++ C/exam-training/ex01.c
@@ -11,38 +11,35 @@ typedef struct
 } Arguments;
 
 Arguments checkArgs(int, char **);
-char* genString(int, char, char);
+char *genString(int, char, char);
+char ***allocateMatrix(int, int, int);
+void fillMatrix(char ***, int, int, int);
+void sortMatrix(char ***, int, int);
+void sortStrings(char **, int);
+void printMatrix(char ***, int, int);
+char *searchWString(char ***, int, int, int, char, int *, int *);
+void replaceChar(char *, char, char);
 
 void main(int argc, char *argv[])
 {
   Arguments arguments = checkArgs(argc, argv);
   int M = arguments.M, N = arguments.N, K = arguments.k;
-  int j, i;
-  char ***S = malloc(M * sizeof(char **));
-
-  for (j = 0; j < M; j++)
-  {
-    S[j] = malloc(N * sizeof(char *));
-    for (i = 0; i < N; i++)
-    {
-      S[j][i] = malloc(sizeof(char) * K);
-      char *randomStr = genString(K, 'a', 'z');
-      strcpy(S[j][i], randomStr);
-    }
-  }
-
-  // printing matrix
-  for (j = 0; j < M; j++)
-  {
-    for (i = 0; i < N; i++)
-      printf("%s\n", S[j][i]);
-    printf("\n");
-  }
+  char w = arguments.w;
+  int i, j;
+  int rowIndex = 0, colIndex = 0;
+  char ***S = allocateMatrix(M, N, K);
+  fillMatrix(S, M, N, K);
+  sortMatrix(S, M, N);
+  printMatrix(S, M, N);
+  
+  char *string = searchWString(S, M, N, K, w, &rowIndex, &colIndex);
+  replaceChar(string, '*', w);
+  printf("The string with most \"%c\" occurrencies is at S[%d][%d]: \"%s\"\n\n", w, rowIndex, colIndex, string);
 
   // free memory
-  for (j = 0; j < M; j++)
-    for (i = 0; i < N; i++)
-      free(S[j][i]);
+  for (i = 0; i < M; i++)
+    for (j = 0; j < N; j++)
+      free(S[i][j]);
 }
 
 Arguments checkArgs(int argc, char **argv)
@@ -65,12 +62,109 @@ Arguments checkArgs(int argc, char **argv)
   }
 }
 
-char* genString(int length, char start, char end) {
-  char* str = malloc(sizeof(char)*length);
+char *genString(int length, char start, char end)
+{
+  char *str = malloc(sizeof(char) * length);
   int i;
   int st = (int)(start);
   int en = ((int)(end)) + 1;
-  for(i = 0; i < length; i++)
-    str[i] = (rand()%(en-st))+st;
+  for (i = 0; i < length; i++)
+    str[i] = (rand() % (en - st)) + st;
   return str;
+}
+
+char ***allocateMatrix(int M, int N, int K)
+{
+  int i, j;
+  char ***matrix = malloc(M * sizeof(char **));
+  for (i = 0; i < M; i++)
+  {
+    matrix[i] = malloc(N * sizeof(char *));
+    for (j = 0; j < N; j++)
+    {
+      matrix[i][j] = malloc(sizeof(char) * K);
+    }
+  }
+  return matrix;
+}
+
+void fillMatrix(char ***matrix, int M, int N, int K)
+{
+  int i, j;
+  for (i = 0; i < M; i++)
+  {
+    for (j = 0; j < N; j++)
+    {
+      char *randomStr = genString(K, 'a', 'z');
+      strcpy(matrix[i][j], randomStr);
+    }
+  }
+}
+
+void sortMatrix(char ***matrix, int M, int N)
+{
+  int i, j;
+  for (i = 0; i < M; i++)
+  {
+    sortStrings(matrix[i], N);
+  }
+}
+
+void sortStrings(char **arr, int length)
+{
+  int i, j;
+  for (i = 0; i < length; i++)
+    for (j = 0; j < length - 1 - i; j++)
+      if (strcmp(arr[j + 1], arr[j]) < 0)
+      {
+        char *temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+}
+
+void printMatrix(char ***matrix, int M, int N)
+{
+  int i, j;
+  for (i = 0; i < M; i++)
+  {
+    for (j = 0; j < N; j++)
+      printf("%s\n", matrix[i][j]);
+    printf("\n");
+  }
+}
+
+char *searchWString(char ***matrix, int M, int N, int K, char w, int *rowIndex, int *colIndex) {
+  char *stringWithMostW;
+  int wMostOccurrencies = 0;
+  int i, j, k;
+  for(i = 0; i < M; i++)
+    for(j = 0; j < N; j++) {
+      int c = 0;
+      for(k = 0; k < K; k++) {
+        if(matrix[i][j][k] == w) {
+          c++;
+        }
+      }
+      if(c > wMostOccurrencies) {
+        wMostOccurrencies = c;
+        stringWithMostW = matrix[i][j];
+        *rowIndex = i;
+        *colIndex = j;
+      }
+    }
+  return stringWithMostW;
+}
+
+void replaceChar(char *string, char c, char toReplace) {
+  int i = 0;
+  int length = strlen(string);
+  while (i < length)
+  {
+    if(string[i] == toReplace) {
+      string[i] = c;
+    } 
+    i++;
+  }
+  
 }
