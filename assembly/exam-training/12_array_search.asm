9d9d0328fea7c061c645af0d1da7a7dbeb316b72
simonecalabrese <simoneca3@gmail.com>
2023-06-19 22:08:29 +0200

    fixes

--- assembly/exam-training/12_array_search.asm
+++ assembly/exam-training/12_array_search.asm
@@ -1,19 +1,21 @@
-total_numbers	dcd		4
-numbers		dcd		1,2,3,4
-			mov		r3,#7 ; n to find
-			mov		r1,#total_numbers
-			ldr		r0, [r1]
-			mov		r1,#numbers
-			mov		r5,#0 ; index of found n
-loop
-			cmp		r0,#-1
+array		dcd		3,10,9,7,3,2,1,4,20
+			mov		r0, #9 ; array size
+			mov		r1, #array
+			mov		r2, #4 ; n to find
+			mov		r3, #-1 ; found n index
+scan_loop
+			cmp		r0, #20
 			beq		end_
-			ldr		r2, [r1]
-			cmp		r3,r2
-			beq		end_
-			sub		r0,r0,#1
+			ldr		r4, [r1]
+			cmp		r4, r2
+			bne		c_scan_loop
+			mov		r5, #array
+			sub		r1,r1,r5
+			lsr		r3,r1,#2
+			b		end_
+c_scan_loop
 			add		r1,r1,#4
-			add		r5,r5,#1
-			b		loop
+			sub		r0,r0,#1
+			b		scan_loop
 end_
 			end
