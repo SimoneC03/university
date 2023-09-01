00abc1e8299d83b8c8bcef5df053aeed6999c2ea
simonecalabrese <simoneca3@gmail.com>
2023-09-01 16:46:59 +0200

    binary tree

--- C++/exam-training/24_binarytree2.cpp
+++ C++/exam-training/24_binarytree2.cpp
@@ -7,6 +7,7 @@ template<typename T>
 struct Node {
     Node<T> *left;
     Node<T> *right;
+    Node<T> *father;
     T value;
 };
 
@@ -15,15 +16,20 @@ class BinaryTree {
 
     private: 
 
+    /* Recursive method to insert a newNode after a given node. */
     void insertAfter(Node<T> *n, Node<T> *newNode) {
         if(newNode == nullptr || n == nullptr) return;
         if(newNode->value < n->value) {
-            if(n->left == nullptr) n->left = newNode;
-            else insertAfter(n->left, newNode);
+            if(n->left == nullptr) {
+                newNode->father = n;
+                n->left = newNode;
+            } else insertAfter(n->left, newNode);
         }
         else {
-            if(n->right == nullptr) n->right = newNode;
-            else insertAfter(n->right, newNode);
+            if(n->right == nullptr) {
+                newNode->father = n;
+                n->right = newNode;
+            } else insertAfter(n->right, newNode);
         }
     }
 
@@ -47,7 +53,17 @@ class BinaryTree {
             newNode->value = value;
             newNode->left = nullptr;
             newNode->right = nullptr;
+            newNode->father = nullptr;
             return newNode;
+        }
+
+        void insertRecursive(T value) {
+            Node<T> *newNode = createNode(value);
+            if(root == nullptr) {
+                root = newNode;
+                return;
+            }
+            insertAfter(root, newNode);
         }
 
         void insert(T value) {
@@ -56,7 +72,30 @@ class BinaryTree {
                 root = newNode;
                 return;
             }
-            insertAfter(root, newNode);
+            Node<T> *current = root;
+            Node<T> *currentFather = nullptr;
+            while(current != nullptr) {
+                currentFather = current;
+                if(value < current->value) current = current->left;
+                else current = current->right;
+            }
+            newNode->father = currentFather;
+            if(value < currentFather->value) currentFather->left = newNode;
+            else currentFather->right = newNode;
+        }
+
+        Node<T> *min(Node<T> *n) {
+            if(root == nullptr) return nullptr;
+            Node<T> *current = n;
+            while(current->left != nullptr) current = current->left;
+            return current;
+        }
+
+        Node<T> *max(Node<T> *n) {
+            if(root == nullptr) return nullptr;
+            Node<T> *current = n;
+            while(current->right != nullptr) current = current->right;
+            return current;
         }
         
         Node<T> *min() {
@@ -76,6 +115,41 @@ class BinaryTree {
         Node<T> *search(T value) {
             if(root == nullptr) return nullptr;
             else return searchAfter(root, value);
+        }
+
+        Node<T> *successor(Node<T> *n) {
+            if(n->right != nullptr)
+                return min(n->right);
+            Node<T> *current = n->father;
+            while(current != nullptr && n == current->right) {
+                n = current;
+                current = current->father;
+            }
+            return current;
+        }
+
+        /* Replace a subtree of root u with a new one of root n */
+        void transplant(Node<T> *u, Node<T> *n) {
+            if(u->father == nullptr) root = n;
+            else if(u == u->father->left) u->father->left = n;
+            else u->father->right = n;
+            if(n != nullptr) n->father = u->father;
+        }
+
+        void deleteNode(Node<T> *n) {
+            if(n->left == nullptr) transplant(n, n->right);
+            else if(n->right == nullptr) transplant(n, n->left);
+            else {
+                Node<T> *m = min(n->right);
+                if(m->father != n) {
+                    transplant(m, m->right);
+                    m->right = n->right;
+                    m->right->father = m;
+                }
+                transplant(n, m);
+                m->left = n->left;
+                m->left->father = m;
+            }
         }
 
         /* Print view of the BinaryTree using the Preorder algorithm */
@@ -125,5 +199,9 @@ int main() {
     Node<int> *found = tree->search(toFind);
     if(found != NULL) cout << "Value " << toFind << " found at " << found << "!\n";
     else cout << "Value "<< toFind << " was not found.\n";
+    cout << "Successor of " << tree->root->right->value << " is: " << tree->successor(tree->root->right)->value << "\n";
+    cout << "Deleting " << tree->root->right->value << "...\n";
+    tree->deleteNode(tree->root->right);
+    tree->print();
     return 0;
 }
