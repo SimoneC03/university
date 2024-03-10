0211864f5c39965751848d9ad5eaae98b7424735
simonecalabrese <simoneca3@gmail.com>
2024-03-10 19:18:37 +0100

    Add parseUsers function

--- network_course/01_08-03-2024/users.c
+++ network_course/01_08-03-2024/users.c
@@ -25,25 +25,54 @@ char *concatenate(User users[], const int n)  {
     if(final == NULL) exit(137);
     for(int i = 0; i < n; i++) {
         strcat(final, users[i].name);
-        final = strcat(final, ";");
-        final = strcat(final, users[i].lastname);
-        final = strcat(final, ";");
+        strcat(final, ";");
+        strcat(final, users[i].lastname);
+        strcat(final, ";");
         char age[10];
         sprintf(age, "%d", users[i].age);
-        final = strcat(final, age);
-        final = strcat(final, ";");
-        final = strcat(final, delimiter);
+        strcat(final, age);
+        strcat(final, ";");
+        strcat(final, delimiter);
     }
     return final;
 }
 
 /**
  * @param users to the concatenated users string
- * @param n number of users 
  * @returns a pointer to the User array
 */
-User *parseUsers(char *users, const int n) {
-    
+User **parseUsers(char *users) {
+    // count users to allocate required space
+    int n = 0;
+    for(size_t i = 0; users[i] != '\0'; i++) {
+        if(users[i] == '-' && users[i+1] == '-') n++;
+    }
+    // allocate final array to return
+    User **users_arr = malloc(sizeof(User*) * n);
+    int user_index = 0; // keep track of insert users
+    char *temp = malloc(sizeof(User)); // save the current parsed user string
+    int temp_i = 0; 
+    for(size_t i = 0; users[i] != '\0'; i++) {
+        if(users[i] == '-' && users[i+1] == '-') {
+            // insert user inside final array
+            char *name = malloc(50);
+            char *lastname = malloc(50);
+            short int *age;
+            sscanf(temp, "%[a-zA-Z0-9];%[a-zA-Z0-9];%hd;", name, lastname, age);
+            User *u = malloc(sizeof(User));
+            strcpy(u->name, name);
+            strcpy(u->lastname, lastname);
+            u->age = *age;
+            users_arr[user_index] = u;
+            user_index++;
+            temp_i = 0;
+            i++;
+        } else {
+            temp[temp_i] = users[i];
+            temp_i++;
+        }
+    }
+    return users_arr;
 }
 
 int main(int argc, char **argv) {
@@ -63,14 +92,14 @@ int main(int argc, char **argv) {
     printf("Users insert. Concatenating...\n");
     // concatenate all the users
     char *concatenated = concatenate(users, n_users);
-    printf("Concatenated users string is: \n%s\nParsing users from string...\n", concatenated);
+    printf("\nConcatenated users string is: \n%s\n\nParsing users from string...\n", concatenated);
     // build `users2` array from the concatenated string
-    // User *users2 = parseUsers(concatenated, n_users);
-    // // print all the parsed users
-    // for(int i = 0; i < n_users; i++) {
-    //     printf("User %d\nName: %s", i+1, users[i].name);
-    //     printf("Lastname: %s", users[i].lastname);
-    //     printf("Age: %s", users[i].age);
-    // }
+    User **users2 = parseUsers(concatenated);
+    // print all the parsed users
+    for(int i = 0; i < n_users; i++) {
+        printf("\nUser %d\nName: %s\n", i+1, users2[i]->name);
+        printf("Lastname: %s\n", users2[i]->lastname);
+        printf("Age: %hd\n", users2[i]->age);
+    }
     return 0;
 }
